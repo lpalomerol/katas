494f946c20a2475a1bd174a37386282368c2c92d
Luis Palomero <luis.palomero@visma.com>
2024-11-18 20:54:14 +0100

    Create new test (red purpousely)

--- katas/tests/testthat/test-fizzbuzz.R
+++ katas/tests/testthat/test-fizzbuzz.R
@@ -19,3 +19,15 @@ test_that("When input is 2 should return '1', '2'", {
   #Assert
   expect_equal(output, expected_output)
 })
+
+
+test_that('when input is 3 should return "1", "2", "fizz"', {
+  # Arrange
+  expected_output = c('1', '2', 'fizz')
+  input = 3
+
+  #Act
+  output = fizzbuzz(input)
+  #Assert
+  expect_equal(output, expected_output)
+})
