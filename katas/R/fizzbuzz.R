0330a24ef8c71d5f02592f11b3b6f3903759570d
Luis Palomero <luis.palomero@visma.com>
2024-11-20 18:15:41 +0100

    Clean code

--- katas/R/fizzbuzz.R
+++ katas/R/fizzbuzz.R
@@ -13,20 +13,11 @@
 #' fizzbuzz(15) -> c('1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz')
 #' }
 #'
+
 fizzbuzz = function(n){
-  return(fizzbuzz_recur(n))
-  # out = c()
-  # for(i in 1:n){
-  #   out = c(out, detect_fizzbuzz(i))
-  # }
-  # return(out)
-
-}
-
-fizzbuzz_recur = function(n){
   if(n == 1){
     return('1')
   }else{
-    return(c(fizzbuzz_recur(n-1), detect_fizzbuzz(n)))
+    return(c(fizzbuzz(n-1), detect_fizzbuzz(n)))
   }
 }
